ae6e5374bd6a6b734e5661bcf5dcbbfda92db9ce
ssmsaito <saito@iflab.tokyo>
2020-11-10 08:59:10 +0000

    certbot fix

--- rev-proxy/entrypoint.sh
+++ rev-proxy/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
-certbot certonly --nginx -d ${DOMAIN_NAME} -m ${EMAIL} --agree-tos -n
-certbot renew
 nginx
+certbot --nginx -d ${DOMAIN_NAME} -m ${EMAIL} --agree-tos -n
+certbot renew
+nginx -s reload
 /bin/ash
