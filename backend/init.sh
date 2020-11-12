86c078651808fa936e168d08127aefd52fa1d38d
ssmsaito <saito@iflab.tokyo>
2020-11-12 17:48:01 +0900

    redis container

--- backend/init.sh
+++ backend/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf ./.pid
-redis-server &
+envsubst '$$REDIS_ADDRESS' < ${BACKEND_HOME}/config_server.ini.template > ${BACKEND_HOME}/config_server.ini
 python -m ducts server start
 /bin/bash
