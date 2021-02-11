15712e814894826d1adcb3fe05093dd4ebe2d3da
ssmsaito <susumu@pcl.cs.waseda.ac.jp>
2021-02-11 10:56:14 +0900

    refactored tutti-new a bit

--- backend/libs/tutti-new.js
+++ backend/libs/tutti-new.js
@@ -4,8 +4,8 @@ window.ducts.tutti._local = window.ducts.tutti._local || {};
 
 window.ducts.tutti.Duct = class extends window.ducts.Duct {
 
-    constructor(wsd) {
-	    super(wsd);
+    constructor() {
+	    super();
 
         this.controllers = {
             resource: new window.ducts.tutti.ResourceController(this),
@@ -27,63 +27,13 @@ window.ducts.tutti.Duct = class extends window.ducts.Duct {
             sent: [],
             received: []
         }
+        this.onOpenHandlers = [];
 
-        this.evtHandlers = {}
-        this.evtHandlersNew = {}
-
-        this.addEvtHandler =
-            ({ tag, eid, handler }) => {
-                if(!(eid in this.evtHandlers)) this.evtHandlers[eid] = {}
-                this.evtHandlers[eid][tag] = handler
-            }
-
-        this.addTuttiEvtHandler =
-            ({ eid, success, error }) => {
-                if(!(eid in this.evtHandlersNew)) this.evtHandlersNew[eid] = [];
-                this.evtHandlersNew[eid].push({ success, error } );
-            }
-            
         this.catchall_event_handler = (rid, eid, data) => {
             if(eid>=1000) this.log.received.push({ rid, eid, data })
-            if(eid in this.evtHandlers) {
-                for(var tag in this.evtHandlers[eid]){
-                    this.evtHandlers[eid][tag](rid, eid, data)
-                }
-            }
-            if(this.evtHandlersNew[eid]) {
-                if(data["Status"]=="Success")
-                    for(var i in this.evtHandlersNew[eid])  this.evtHandlersNew[eid][i].success({ rid, eid, timestamp: data["Timestamp"], data: data["Data"] } );
-                else if (data["Status"]=="Error") {
-                    console.error("Tutti event error:", data["Reason"] );
-                    for(var i in this.evtHandlersNew[eid]) {
-                        if(this.evtHandlersNew[eid][i].error) {
-                            this.evtHandlersNew[eid][i].error({ rid, eid, timestamp: data["Timestamp"], data: data } );
-                        }
-                    }
-                }
-            }
         }
 
-        this.onOpenHandlers = [];
-        this.addOnOpenHandler = (handler) => {
-            this.onOpenHandlers.push(handler);
-        };
-
-        this._connection_listener.on("onopen", () => {
-            this.setupHandlers(this);
-            for(var i in this.onOpenHandlers){
-                this.onOpenHandlers[i]( );
-            }
-        } );
-
-
-    }
-
-    setTuttiEventHandler(eid, success, error) {
-        this.setEventHandler(eid, (rid, eid, data) => {
-            if (data["Status"]=="Success") success({ rid, eid, timestamp: data["Timestamp"], data: data["Data"] } );
-            else if (data["Status"]=="Error" && error) error({ rid, eid, timestamp: data["Timestamp"], reason: data["Reason"] } );
-        } );
+        this.addOnOpenHandler = (handler) => { this.onOpenHandlers.push(handler); };
     }
 
     invokeOrWaitForOpen(f) {
@@ -93,14 +43,11 @@ window.ducts.tutti.Duct = class extends window.ducts.Duct {
  
     _onopen(self, event) {
 	    super._onopen( self, event );
-        self.addEvtHandler({
-            tag: "",
-	        eid: self.EVENT.APP_WSD,
-	        handler: (rid, eid, data) => {
-                self.APP_WSD = data
-	        }
-        } );
+        self.setEventHandler( self.EVENT.APP_WSD, (rid, eid, data) => { self.APP_WSD = data } );
 	    self.send( self.next_rid(), self.EVENT.APP_WSD, null );
+
+        this.setupHandlers(this);
+        for(const handler of this.onOpenHandlers)  handler();
     }
 
     // FIXME:: needs a protocol
