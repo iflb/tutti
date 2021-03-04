927e82bdba38663a864dfd430a744f099aaf47e7
ssmsaito <saito@iflab.co.jp>
2021-03-04 12:02:03 +0900

    watchmedo excluding projects dir

--- backend/init.sh
+++ backend/init.sh
@@ -2,6 +2,5 @@
 
 rm -rf ./.pid
 envsubst '$$REDIS_ADDRESS' < ${BACKEND_HOME}/config_server.ini.template > ${BACKEND_HOME}/config_server.ini
-#python -m ducts server start
-#/bin/bash
-watchmedo shell-command -W -R -p '*.py' -c 'python -m ducts server stop && python -m ducts server start' ./
+python -m ducts server start &
+watchmedo shell-command -W -R -p '*.py' --ignore-patterns="projects/*/*" -c 'echo ${watch_src_path}; python -m ducts server stop && python -m ducts server start' ./
