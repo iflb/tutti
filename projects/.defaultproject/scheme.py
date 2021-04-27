aa21ce7cb41569b3068d7f0e0c5e0d070d68ee1b
ssmsaito <saito@iflab.co.jp>
2021-04-27 18:11:27 +0900

    updated defaultproject scheme.py

--- projects/.defaultproject/scheme.py
+++ projects/.defaultproject/scheme.py
@@ -4,11 +4,17 @@ from libs.scheme.flow import BatchNode, TemplateNode, Statement
 class ProjectScheme(ProjectSchemeBase):
     def config_params(self):
         self.title = ""
-        # self.assignment_order = "bfs"
-        # self.sort_order = "natural"
-        # self.pagination = False
-        # self.instruction = True
-        # self.show_title = True
-        
+        #self.assignment_order = "bfs"
+        #self.sort_order = "natural"
+        #self.show_title = True
+        #self.page_navigation = False
+        #self.push_instruction = True
+        #self.instruction_btn = True
+        #self.allow_parallel_sessions = True
+        #self.anonymous = False
+        #self.preview = True
+        #self.completion_alert = False
+
     def define_flow(self):
         # define your task flow here
+        pass
